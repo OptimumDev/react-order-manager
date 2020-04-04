82ec7ff0be1dff3492cf36f29a6370e083eb6b7e
Artemiy <38810090+OptimumDev@users.noreply.github.com>
2020-04-04 15:17:13 +0500

    add persist store for saving

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,14 +6,29 @@ import {Provider} from "react-redux";
 import {rootReducer} from "../../Reducers";
 import Days from "../../Containers/Days";
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/lib/integration/react';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer, applyMiddleware(logger));
+const persistor = persistStore(store);
+
 
 function App() {
     return (
         <Provider store={store}>
-            <div className="App">
-                <Days />
-            </div>
+            <PersistGate loading={null} persistor={persistor}>
+                <div className="App">
+                    <Days />
+                </div>
+            </PersistGate>
         </Provider>
     );
 }
