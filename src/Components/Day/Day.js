8a9484b9725d49962a29d4c05dff3f7b1c772fa2
Artemiy <38810090+OptimumDev@users.noreply.github.com>
2020-04-09 01:39:54 +0500

    add statistics box

--- src/Components/Day/Day.js
+++ src/Components/Day/Day.js
@@ -2,6 +2,7 @@ import React from "react";
 import './Day.css'
 import {ReactSortable} from "react-sortablejs";
 import Order from '../../Containers/Order'
+import DayStatistics from "../DayStatistics/DayStatistics";
 
 export default function Day(props) {
     return (
@@ -10,17 +11,20 @@ export default function Day(props) {
                 {new Date(props.dateStr)
                     .toLocaleDateString('ru-RU', {year: 'numeric', month: 'long', day: 'numeric'})}
             </div>
-            <ReactSortable
-                className='orders'
-                group='orders'
-                animation={300}
-                list={props.orderIds.map(id => ({id}))}
-                setList={items => props.setOrderIds(items.map(item => item.id))}
-            >
-                {props.orderIds.map(id =>
-                    <Order orderId={id} key={id}/>
-                )}
-            </ReactSortable>
+            <div className='day-data'>
+                <ReactSortable
+                    className='orders'
+                    group='orders'
+                    animation={300}
+                    list={props.orderIds.map(id => ({id}))}
+                    setList={items => props.setOrderIds(items.map(item => item.id))}
+                >
+                    {props.orderIds.map(id =>
+                        <Order orderId={id} key={id}/>
+                    )}
+                </ReactSortable>
+                <DayStatistics/>
+            </div>
         </div>
     );
 }
---
+++ src/Components/DayStatistics/DayStatistics.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './DayStatistics.css'
+
+export default function DayStatistics() {
+    return (
+        <div className='day-statistics'>Some statistics</div>
+    )
+}
