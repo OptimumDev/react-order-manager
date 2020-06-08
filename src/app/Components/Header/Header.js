1a7280c8780f9a2c9c0d08f6fd299ea112b55b7f
Artemiy <38810090+OptimumDev@users.noreply.github.com>
2020-06-08 17:58:51 +0500

    fix tabs

--- src/app/Components/Header/Header.js
+++ src/app/Components/Header/Header.js
@@ -7,14 +7,16 @@ import * as PageNames from "../../Constants/PageNames";
 export default function Header({currentPage, datesToCreate, onPageChange, onOrderCreate}) {
     return (
         <header className='app-header'>
-            <span className='app-name'>Order Manager</span>
-            <Controls datesToCreate={datesToCreate} onOrderCreate={onOrderCreate}/>
+            <div className='app-name-container'>
+                <span className='app-name'>Order Manager</span>
+            </div>
             <nav className='tabs'>
                 <Tabs value={currentPage} onValueChange={onPageChange}>
                     <Tabs.Tab id={PageNames.CURRENT_ORDERS}>Текущие</Tabs.Tab>
                     <Tabs.Tab id={PageNames.DONE_ORDERS}>Выполненые</Tabs.Tab>
                 </Tabs>
             </nav>
+            <Controls datesToCreate={datesToCreate} onOrderCreate={onOrderCreate}/>
         </header>
     );
 }
