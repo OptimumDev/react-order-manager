2cd725bfd0fd072944cabae141248e3d28d9a1af
Artemiy <38810090+OptimumDev@users.noreply.github.com>
2020-06-09 21:03:43 +0500

    upgrade statistics

--- src/app/Components/DayStatistics/DayStatistics.js
+++ src/app/Components/DayStatistics/DayStatistics.js
@@ -1,9 +1,11 @@
 import React from "react";
 import './DayStatistics.css'
 import {fieldProps} from "../../Constants/OrderFieldProps";
-import {sum} from "../../Utils/arrayHelper";
+import {colors, textColors} from "../../Constants/Colors";
 
 export default function DayStatistics({orders}) {
+    const statistics = getStatistics(orders);
+    console.log(statistics)
     return (
         <div className='day-statistics'>
             <table className='order-data'>
@@ -11,14 +13,50 @@ export default function DayStatistics({orders}) {
                 <tbody>
                 <tr>
                     <th>{fieldProps.quantity.name}</th>
-                    <td>{sum(orders, o => o.quantity)}</td>
+                    <td>{statistics.quantity}</td>
                 </tr>
                 <tr>
                     <th>{fieldProps.area.name}</th>
-                    <td>{sum(orders, o => o.area)}</td>
+                    <td>{statistics.area}</td>
+                </tr>
+                <tr>
+                    <th>Заказов</th>
+                    <td>{statistics.count}</td>
                 </tr>
                 </tbody>
             </table>
+            <div className='color-counts'>
+                {getColorCounts(statistics.byColor)}
+            </div>
         </div>
-    )
+    );
+}
+
+function getColorCounts(byColor) {
+    return Object.entries(byColor)
+        .filter(([_, count]) => count > 0)
+        .map(([color, count]) => (
+            <span style={{backgroundColor: color, color: textColors[color]}} className='color-count'>
+                {count}
+            </span>
+        ));
+}
+
+function getStatistics(orders) {
+    return orders.reduce((acc, order) => {
+        acc.count += 1;
+        acc.quantity += order.quantity;
+        acc.area += order.area;
+        acc.byColor[order.color] += 1;
+
+        return acc;
+    }, {
+        count: 0,
+        quantity: 0,
+        area: 0,
+        byColor: colors.reduce((acc, color) => {
+            acc[color] = 0;
+            return acc;
+        }, {})
+    });
 }
